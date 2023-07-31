2424cc83f5904f1ac67f49b6e877a25d12299d59
MuhammadAsif46 <asifahmed32c@gmail.com>
2023-07-31 23:29:40 +0500

    Updated app.js

---
+++ app.js
@@ -0,0 +1,15 @@
+//********* Question 1 ********/
+
+// var num;  
+// var character = prompt("enter a character.");
+
+// if (character === "A") {
+//     num = 65;
+// } else if (character === "a") {
+//     num = 97;
+// }else if (character === "Z"){
+//     num = 90; 
+// }else if (character === "z"){
+//     num = 122; 
+// }
+// alert(character + " = " + num);
