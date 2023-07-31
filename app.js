d396bbecddac28d2bdeb372184b0c309815a2eaf
MuhammadAsif46 <asifahmed32c@gmail.com>
2023-07-31 23:30:31 +0500

    Updated app.js

--- app.js
+++ app.js
@@ -13,3 +13,19 @@
 //     num = 122; 
 // }
 // alert(character + " = " + num);
+
+
+// ******** Take input from user and show in which integers is largest ********//
+
+// var n1 = +prompt("ENTER FIRST NUMBER");
+// var n2 = +prompt("ENTER SECOND NUMBER");
+
+// if (n1 > n2 ){
+//     n1 = " the first number " + n1 + " is greater than the second number " + n2 ;
+// }else if (n1 < n2){
+//   n1 = " the first number " + n1 + " is less than the second number " + n2 ;
+// }
+// else if (n1 === n2){
+//   n1 = " the first number " + n1 + " is equal than the second number " + n2 ;
+// }
+// alert(n1);
